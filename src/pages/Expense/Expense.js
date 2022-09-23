c9cd919ac8220f18322e931494b727f01eb868f9
zainuleb <zainulebadd@gmail.com>
2022-09-23 20:27:33 +0500

    correcting component structure

--- src/pages/Expense/Expense.js
+++ src/pages/Expense/Expense.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ExpenseConfigTable from '../../components/ExpenseConfigurations/ExpenseConfigTable';
-
+import ExpensesTypeForm from '../../components/ExpenseConfigurations/ExpensesTypeForm';
 import './style.scss';
 import AdminHeading from '../../components/AdminHeading/AdminHeading';
-import ExpensesTypeForm from '../../components/ExpensesConfigForm/ExpensesTypeForm';
 
 const Expense = () => {
   return (
